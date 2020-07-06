d82498b79b0d2cbcafd857c4e02780d5f948bf77
Diamanterub <40190163@esmad.ipp.pt>
2020-07-06 16:09:32 +0100

    Alpha 0.2.2

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Config necessary for the bot run
 const {
     Client
 } = require("discord.js");
@@ -8,76 +9,78 @@ const {
 const client = new Client({
     disableEveryone: true
 });
-
-config({
-
-    path: __dirname + "/.env"
-
-});
+//If the bot is ready to go online he sends a message to the console and sets his precence to the desired one
 client.on("ready", () => {
     console.log("BOT initiated BEEP BOOP , my name is " + client.user.username);
 
     client.user.setPresence({
         game: {
-            name: "RUB GO MAD OVER JAVASCRIPT",
-            type: "WATCHING"
+            name: "Imagine losing your mind for me yikes",
+           // type: "WATCHING"
         }
     });
 });
-
+//Fuction for generating a random value between two numbers
 function getRnd(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
+//Prefix of the bot 
 const prefix = "_";
-
+//The prefix one
 client.on("message", async message => {
 
-
-    if (message.channel.type != 'text' || message.author.bot)
-        return;
+    //Checklist for the code to run
+    //What is sent for the bot needs to be text,and needs to have the prefix for the bot 
+    if (message.channel.type != 'text' || message.author.bot) return;
     if (!message.guild) return; /* ANTI ERROR NAS DMS*/
-
     if (!message.content.startsWith(prefix)) return;
+    //Getting the text for the message sent
     let args = message.content.slice(prefix.length).trim().split(/ +/g);
     let cmd = args.shift().toLowerCase();
 
+    //All the options that are available with the prefix 
     switch (cmd) {
-        case 'ping': {
+
+        //Ping option for checking possible problems with the api or bot 
+        case 'ping': 
             let msg = await message.channel.send('Ping');
             msg.edit('Ping:\nLatency is' + Math.floor(msg.createdTimestamp - message.createdTimestamp) + 'ms \n API Latency is ' + Math.round(client.ping) + 'ms');
 
-        }
+        
         break;
-    case 'restart': {
-        let msg = message.channel.send('Restarting...').then(m => {
-            client.destroy().then(() => {
-                client.login(process.env.TOKEN);
-                msg.edit = "Restarted"
-            });
 
-        });
-    }
-    break;
-    case 'crab': {
-
+        //Crab option that send the gif of the crab when the user requests
+        case 'crab': 
         message.channel.send("https://tenor.com/view/crab-safe-dance-gif-13211112");
-    }
-    break;
-    case 'champion': {
+        
+        break;
+
+        //Champion option that gets the fuction getRnd that randomizes a number and choses a champion from the array champions
+        case 'champion': 
         let rnd = getRnd(1, 148)
         let champions = ['Aatrox', 'Ahri', 'Akali', 'Alistar', 'Amumu', 'Anivia', 'Annie', 'Aphelios', 'Ashe', 'Aurelion Sol', 'Azir', 'Bard', 'Blitzcrank', 'Brand', 'Braum', 'Caitlyn', 'Camille', 'Cassiopeia', 'Cho gath', 'Corki', 'Darius', 'Diana', 'Draven', 'Dr.Mundo', 'Ekko', 'Elise', 'Evelynn', 'Ezreal', 'Fiddlesticks', 'Fiora', 'Fizz', 'Galio', 'Gangplank', 'Garen', 'Gnar', 'Gragas', 'Graves', 'Hecarim', 'Heimerdinger', 'Illaoi', 'Irelia', 'Ivern', 'Jhin', 'Jinx', 'Kaisa', 'Kalista', 'Karma', 'Karthus', 'Kassadin', 'Katarina', 'Kayle', 'Kayn', 'Kennen', 'Khazix', 'Kindred', 'Kled', 'Kog maw', 'Leblanc', 'Lee Sin', 'Leona', 'Lissandra', 'Lucian', 'Lulu', 'Lux', 'Malphite', 'Malzahar', 'Maokai', 'Master yi', 'Miss Fortune', 'Mordekaiser', 'Morgana', 'Nami', 'Nasus', 'Nautilus', 'Neeko', 'Nidalee', 'Nocturne', 'Nunu', 'Olaf', 'Orianna', 'Ornn', 'Pantheon', 'Poppy', 'Pyke', 'Qiyana', 'Quinn', 'Rakan', 'Rammus', 'Reksai', 'Renekton', 'Rengar', 'Riven', 'Rumble', 'Ryze', 'Sejuani', 'Senna', 'Sett', 'Shaco', 'Shen', 'Shyvanna', 'Singed', 'Sion', 'Sivir', 'Skarner', 'Sona', 'Soraka', 'Swain', 'Sylas', 'Syndra', 'Tahm Kench', 'Taliyah', 'Talon', 'Taric', 'Teemo', 'Thresh', 'Tristana', 'Trundle', 'Tryndamere', 'Twisted Fate', 'Twitch', 'Udry', 'Urgot', 'Varus', 'Vayne', 'Veigar', 'Vel koz', 'Vi', 'Viktor', 'Vladimir', 'Volibear', 'Warwick', 'Wukong', 'Xayah', 'Xerath', 'Xin Zhao', 'Yasuo', 'Yorick', 'Yummi', 'Zac', 'Zed', 'Ziggs', 'Zilean', 'Zoe', 'Zyra']
         message.channel.send('Random Champion is ' + champions[rnd])
-    }
-    break;
-    case 'porn': {
+        
+        break;
+
+        //Porn option that gets a random porn video from pornhub
+        case 'porn': 
 
         message.channel.send("https://www.pornhub.com/random")
 
-    }
-    break;
+        break;
+        //Cs option to call everyone who plays cs
+        case 'cs':
+            message.channel.send("Can you guys play cs", {
+                tts: true
+                });
+            message.channel.send("<@198930470061211648><@198869765039718400><@177175891078676482><@160788066678734849><@198887105143767040><@195638742864625664>")    
+         break;
+            
 
-    case 'rune': {
+            
+
+     case 'rune': {
 
         let toni = 1;
         let path = ['Precision', 'Domination', 'Sorcery', 'Resolve', 'Inspiration'];
@@ -401,14 +404,18 @@ client.on("message", async message => {
 
 
 });
+//Ban and kick options only avaibable for the admins and botcontrollers respectively
 client.on('message', message => {
+    //CHANGE LATER WHEN BOT IS READY
+    //The ids from the certain roles
     let botcontroller = message.member.roles.has('217716503729012736');
     let admin = message.member.roles.has('217716522808901632')
     let args = message.content.substring(prefix.length).split(" ");
+
     switch (args[0]) {
+        //Kick option only avaibable to the bot controllers
         case 'kick':
             if (botcontroller) {
-
 
                 if (!args[1]) message.channel.send('DIGA ME UM!')
 
@@ -428,20 +435,21 @@ client.on('message', message => {
 
                     }
 
-
                 }
-            } else {
+            }
+            //In case that you try to ban a user that is more powerfull than you
+            else {
                 message.channel.send("You don't have enough power to this command")
 
             }
 
-            break;
+        break;
+
         case 'ban':
+            //Ban option only avaibable to the Admins
             if (admin) {
 
-
                 if (!args[1]) message.channel.send('DIGA ME UM!')
-
                 const user = message.mentions.users.first();
                 if (user) {
                     const member = message.guild.member(user);
@@ -457,57 +465,57 @@ client.on('message', message => {
 
                     }
 
-
                 }
-            } else {
+            }
+            //Ban option only avaibable to the Admins
+            else {
                 message.channel.send("You don't have enough power to this command")
-
             }
 
-            break;
+        break;
 
-        default:
-
-
-            break;
-    }
-
+        }
 })
 
-
+//Test detection used for detecting certain words from normal text 
 client.on("message", async message => {
-    var mech = 0;
-    if (message.channel.type != 'text' || message.author.bot)
-        return;
+   //Checklist necessary for the bot to run
+    if (message.channel.type != 'text' || message.author.bot) return;
     if (!message.guild) return; /* ANTI ERROR NAS DMS*/
-    /*Rework!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
     /*The bot divides the message that the user who made the request  and search for the trigger words and then he tells the user to stfu*/
-    let xtr = message.content;
+    let messageraw = message.content;
     let autor = message.author;
-    let zek = xtr.trim().toLowerCase();
-    let zeksplit = zek.split(' ');
-    var resposta;
-    resposta = (Boolean(zeksplit.includes("outlander")) || Boolean(zeksplit.includes("jojo")));
-    var resposta1;
-    resposta1 = (Boolean(zeksplit.includes("oof")))
-    console.log(xtr);
-    console.log(resposta);
-    console.log(zeksplit);
-    if (Boolean(resposta)) {
-        if (mech === 0) {
+    let messagetext = messageraw.trim().toLowerCase();
+    let messagesplit = messagetext.split(' ');
+    for (i = 0; i < messagesplit.length; i++) {
+     
+        switch(messagesplit[i])
+       {
+        //In case the member writes 'outlander' 
+        case Boolean(messagesplit[i].includes("outlander")):
+
             message.channel.send(autor + " " + "shut the fuck up", {
                 tts: true
-            });
-            mech = 1;
-        }
+                });
+            
+        break;  
+        //In case the member writes 'jojo'
+        case Boolean(messagesplit[i].includes("jojo")):
 
+            message.channel.send(autor + " " + "shut the fuck up", {
+                tts: true
+                });
+
+        break;
+          //In case the member writes 'oof'
+        case Boolean(zeksplit.includes("oof")):
+            
+            message.channel.send("OOF!")
+
+        break;
+
+       }  
     }
-    if (Boolean(resposta1)) {
-        message.channel.send("OOF!")
-
-    }
-
-
 })
 
 client.login(process.env.token);
