6dafc06fb95a1a467392aa5a8fc6cd23b42fde57
Rúben ferreira <rubenferreira812@gmail.com>
2022-08-18 15:51:59 +0100

    Attempts to put the bot working

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Config necessary for the bot run
 const { Client } = require("discord.js");
-const { config } = require("dotenv");
-var cron = require("cron");
+const dotenv = require('dotenv');
+dotenv.config();
 
 const client = new Client({
   disableEveryone: true,
@@ -24,73 +24,69 @@ let autor;
 //Ban and kick options only avaibable for the admins and botcontrollers respectively
 
 client.on("message", (message) => {
-  //CHANGE LATER WHEN BOT IS READY
-  //The ids from the certain roles
-  let botcontroller = message.member.roles.has("417744133994708995");
-  let admin = message.member.roles.has("<688827881127804999");
   let args = message.content.substring(prefix.length).split(" ");
   autor = message.author;
   switch (args[0]) {
     //Kick option only avaibable to the bot controllers
-    case "kick":
-      if (botcontroller) {
-        if (!args[1]) message.channel.send("DIGA ME UM!");
+    // case "kick":
+    //   if (botcontroller) {
+    //     if (!args[1]) message.channel.send("DIGA ME UM!");
 
-        const user = message.mentions.users.first();
-        if (user) {
-          const member = message.guild.member(user);
-          if (member) {
-            member
-              .kick("Kicked!")
-              .then(() => {
-                message.reply(
-                  "The member" + " " + user.tag + " " + "was kicked"
-                );
-              })
-              .catch((error) => {
-                message.reply("I was unable to kick the member");
-                console.log(error);
-              });
-          } else {
-            message.reply("That member is not in the server");
-          }
-        }
-      }
-      //In case that you try to ban a user that is more powerfull than you
-      else {
-        message.channel.send("You don't have enough power to this command");
-      }
+    //     const user = message.mentions.users.first();
+    //     if (user) {
+    //       const member = message.guild.member(user);
+    //       if (member) {
+    //         member
+    //           .kick("Kicked!")
+    //           .then(() => {
+    //             message.reply(
+    //               "The member" + " " + user.tag + " " + "was kicked"
+    //             );
+    //           })
+    //           .catch((error) => {
+    //             message.reply("I was unable to kick the member");
+    //             console.log(error);
+    //           });
+    //       } else {
+    //         message.reply("That member is not in the server");
+    //       }
+    //     }
+    //   }
+    //   //In case that you try to ban a user that is more powerfull than you
+    //   else {
+    //     message.channel.send("You don't have enough power to this command");
+    //   }
 
-      break;
+    //   break;
 
-    case "ban":
-      //Ban option only avaibable to the Admins
-      if (admin) {
-        if (!args[1]) message.channel.send("DIGA ME UM!");
-        const user = message.mentions.users.first();
-        if (user) {
-          const member = message.guild.member(user);
-          if (member) {
-            member
-              .ban({
-                reason: "Imagine being Banned by a bot ",
-              })
-              .then(() => {
-                message.reply(
-                  "The member" + " " + user.tag + " " + "was BANNED"
-                );
-              });
-          } else {
-            message.reply("That member is not in the server");
-          }
-        }
-      }
-      //Ban option only avaibable to the Admins
-      else {
-        message.channel.send("You don't have enough power to this command");
-      }
+    // case "ban":
+    //   //Ban option only avaibable to the Admins
+    //   if (admin) {
+    //     if (!args[1]) message.channel.send("DIGA ME UM!");
+    //     const user = message.mentions.users.first();
+    //     if (user) {
+    //       const member = message.guild.member(user);
+    //       if (member) {
+    //         member
+    //           .ban({
+    //             reason: "Imagine being Banned by a bot ",
+    //           })
+    //           .then(() => {
+    //             message.reply(
+    //               "The member" + " " + user.tag + " " + "was BANNED"
+    //             );
+    //           });
+    //       } else {
+    //         message.reply("That member is not in the server");
+    //       }
+    //     }
+    //   }
+    //   //Ban option only avaibable to the Admins
+    //   else {
+    //     message.channel.send("You don't have enough power to this command");
+    //   }
 
-      break;
+    //   break;
 
     //Ping option for checking possible problems with the api or bot
     case "ping":
