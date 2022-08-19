8ab45a275b0cb8774608f8c5d1c2b7a7415385f4
Rúben ferreira <rubenferreira812@gmail.com>
2022-08-19 13:06:01 +0100

    Events

--- index.js
+++ index.js
@@ -17,10 +17,19 @@ for (const file of commandFiles) {
 	client.commands.set(command.data.name, command);
 }
 
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
-});
+//Setting up the events
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
 client.user.setPresence({
     game: {
@@ -28,21 +37,6 @@ client.user.setPresence({
       type: "WATCHING"
     },
   });
-
-  client.on('interactionCreate', async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-});
 
 // Login to Discord with your client's token
 client.login(process.env.token);
